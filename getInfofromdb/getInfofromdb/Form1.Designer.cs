f24aceb196f612d2c03e120349587b3502d2b21f
JCHutchinson <jonathan.hutchinson@ymail.com>
2015-11-28 12:27:37 -0600

    Get Info From Db
    Initiall test project to get images and other data from Database, and
    display them in windows form. Everything s working at this point
    including the navigation steps. IR remote not incorporated yet.

---
+++ getInfofromdb/getInfofromdb/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace getInfofromdb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.PreviousButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(387, 166);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(259, 244);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(263, 260);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(129, 202);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 4;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(129, 232);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTitle.TabIndex = 5;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(296, 200);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 6;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // PreviousButton
+            // 
+            this.PreviousButton.Location = new System.Drawing.Point(395, 199);
+            this.PreviousButton.Name = "PreviousButton";
+            this.PreviousButton.Size = new System.Drawing.Size(75, 23);
+            this.PreviousButton.TabIndex = 7;
+            this.PreviousButton.Text = "Previous";
+            this.PreviousButton.UseVisualStyleBackColor = true;
+            this.PreviousButton.Click += new System.EventHandler(this.PreviousButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 545);
+            this.Controls.Add(this.PreviousButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button PreviousButton;
+    }
+}
+
---
+++ getInfofromdb/getInfofromdb/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace getInfofromdb
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        string connString;
+        MySqlDataAdapter adapter;
+        MySqlDataAdapter adapter2;
+        MySqlCommand command;
+
+        MemoryStream memStream;
+        DataTable dt;
+        DataSet ds;
+
+        //used to navigate between pivtures
+        int navigate = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //CONNECT TO SERVER
+            connString = "SERVER = 160.153.33.38;PORT=3306;DATABASE=familytv_database1;UID=familyTvOwner;PASSWORD=familyTvProject";
+            //GET IMAGES FUNCTION THAT CONTROLS RETIREVEING AND DISPLAYING IMAGES FROM dATABASE
+         
+            
+        }
+
+        private void getData()
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM familytv_database1.pictures.title",conn);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "pictures");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = connString;
+                conn.Open();
+                MessageBox.Show("connection success");
+
+                adapter = new MySqlDataAdapter("SELECT * FROM familytv_database1.pictures", conn);
+                ds = new DataSet();
+                adapter.Fill(ds, "pictures");
+                dataGridView1.DataSource = ds.Tables["pictures.title"];
+                conn.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = connString;
+                conn.Open();
+                //MessageBox.Show("connection success");
+                
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //string query = "SELECT * FROM UserPics WHERE userid = 12";
+            string query = "SELECT image FROM UserPics2 WHERE userid = 12 ";
+            command = new MySqlCommand(query, conn);
+            adapter2 = new MySqlDataAdapter(command);
+
+            dt = new DataTable();
+            adapter2.Fill(dt);
+
+           // textBoxId.Text = dt.Rows[0][1].ToString();
+           // textBoxTitle.Text = dt.Rows[0][2].ToString();
+
+            byte[] img = (byte[])dt.Rows[0][0];
+            memStream = new MemoryStream(img);
+            pictureBox1.Image = Image.FromStream(memStream);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            //adapter2.Dispose();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            navigate += 1;
+            if (navigate == 4)
+            {
+                navigate = 0;
+            }
+
+            navigateImages(navigate);
+        }
+
+        private void PreviousButton_Click(object sender, EventArgs e)
+        {
+            navigate -= 1;
+            if (navigate < 0)
+            {
+                navigate = dt.Rows.Count-1;
+            }
+            navigateImages(navigate);
+            
+        }
+
+        private void navigateImages(int nav)
+        {
+           // textBoxId.Text = dt.Rows[nav][1].ToString();
+            //textBoxTitle.Text = dt.Rows[nav][2].ToString();
+
+            byte[] img = (byte[])dt.Rows[nav][0];
+            memStream = new MemoryStream(img);
+            pictureBox1.Image = Image.FromStream(memStream);
+        }
+   
+    }
+}
